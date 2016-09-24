52f9d2bd2a7904ffd4ee6e16407fc4b28b7449b2
xiazhibin <xiazhibin@shinemi.cn>
2016-09-24 18:42:46 +0800

    weather api

--- api/weather/__init__.py
+++ api/weather/__init__.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+# coding=utf-8
+from flask import Blueprint, make_response
+import requests
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 weather_bp = Blueprint('weather', __name__, url_prefix='/api/weather')
 
+weather_url_prefix = 'http://api.map.baidu.com/telematics/v3/weather?location=#&output=json&ak=ZLUIqERpIcPUhOK9KVCUdNBa2upZttv8'
 
-@weather_bp.route('/')
-def index():
-    return 'fuxk'
+
+@weather_bp.route('/<string:city>')
+def index(city):
+    url = weather_url_prefix.replace('#', city)
+    r = requests.get(url)
+    response = make_response(r.content)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from api import app
 
-app.run()
+app.run(debug=True)
 
